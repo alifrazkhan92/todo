1bee655e0b3435e94e0eff11eca0d9d04f655948
Ali Fraz <alifrazkhan92@gmail.com>
2024-02-22 17:44:56 +0000

    CRUD

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import list
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        
+        if form.is_valid():
+            form.save()
+            all_items = list.objects.all
+            messages.success(request, ('Thank you for adding an item into the list.'))
+            return render(request, 'home.html', {'all_items':all_items})
+    
+    else:
+        all_items = list.objects.all
+        return render(request, 'home.html', {'all_items':all_items})
 
 
 def about(request):
-    return render(request, 'about.html', {})
+    context = {'firstname' : 'Ali', 'lastname' : 'Fraz'}
+    return render(request, 'about.html', context)
+
+
+def delete(request, list_id):
+    item = list.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('You deleted an item.'))
+    return redirect('home')
